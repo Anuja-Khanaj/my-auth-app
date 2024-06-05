b1962a8975a8a2dc90e3b3aaee2cc005a384b972
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2024-06-05 22:43:35 +0530

    name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyAuthApp
+# AngularfirebaseAuthentication
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBFWOuZvFUIY9gedIESEY3DYZn8XCYS6kg",
+  authDomain: "angularhttp-ed902.firebaseapp.com",
+  databaseURL: "https://angularhttp-ed902-default-rtdb.firebaseio.com",
+  projectId: "angularhttp-ed902",
+  storageBucket: "angularhttp-ed902.appspot.com",
+  messagingSenderId: "584565068252",
+  appId: "1:584565068252:web:7a29c0e7c74e810f44bf19",
+  measurementId: "G-5TD9M35HNE"
+    }
+  };
